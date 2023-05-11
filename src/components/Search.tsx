fb23c802ff6c4546313b20b69d16c9ef899a7af3
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-05-11 23:38:38 +0100

    add images to search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,46 @@
-import { useState } from "react";
-import { PokemonData } from "./types/types";
+import { useState, useEffect } from "react";
+import { PokemonData, PokemonType } from "./types/types";
 import { Link, useLoaderData } from "react-router-dom";
+
+import '../../public/css/search.css';
+
+type PropsType = {
+    pokemons: PokemonData,
+    searchQuery: string
+}
+
+function PokemonList({pokemons, searchQuery}: PropsType) {
+    const [pokemonObject, setPokemonObject] = useState<PokemonType[]>()
+
+    useEffect(() => {
+        Promise.all(pokemons.results.filter((value, index) => {
+            if (searchQuery.length < 3) {
+                return index < 10;
+            }
+            return value.name.includes(searchQuery.toLowerCase());
+        })
+        .map(async pokemon => {
+            const pokemonObject = await (await fetch(`${pokemon.url}`)).json() as PokemonType;
+    
+            return pokemonObject;
+        }))
+        .then(value => setPokemonObject(value))
+    }, [searchQuery])
+
+
+    return (
+        <>
+            {
+                pokemonObject?.map(pokemon => {
+                    return <div key={pokemon.name} className="search-link-div">
+                        <img src={pokemon.sprites.front_default} style={{visibility: (!pokemon.sprites.front_default ? 'hidden' : 'visible')}} width="56" height="56"/>
+                        <Link to={`pokemon/${pokemon.name}`} style={{ display: "block" }}>{pokemon.name}</Link>
+                    </div>
+                })
+            }
+        </>
+    )
+}
 
 export default function Search() {
     const [searchQuery, setSearchQuery] = useState('');
@@ -10,20 +50,14 @@ export default function Search() {
     return (
         <>
             <section>
-                <div>
-                    <input type="text" onChange={e => setSearchQuery(e.target.value)} placeholder="search for a pokemon" />
-                    <button type="submit">search</button>
-                </div>
-                <div>
-                    {
-                        pokemons.results.filter((value, index) => {
-                            if(searchQuery.length < 3) {
-                                return index < 10;
-                            }
-                            return value.name.includes(searchQuery);
-                        })
-                        .map(pokemon => <Link key={pokemon.name} to={`pokemon/${pokemon.name}`} style={{display: "block"}}>{pokemon.name}</Link>)
-                    }
+                <div id="search-div">
+                    <div>
+                        <input type="text" onChange={e => setSearchQuery(e.target.value)} placeholder="search for a pokemon" />
+                        <button type="submit">search</button>
+                    </div>
+                    <div id="pokemon-list">
+                        <PokemonList pokemons={pokemons} searchQuery={searchQuery} />
+                    </div>
                 </div>
             </section>
         </>
