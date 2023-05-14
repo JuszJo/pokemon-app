1a25b467106074dfbd2febf287327eb5e28df26a
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-05-14 15:28:45 +0100

    add new styles

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -7,6 +7,8 @@ import '../../public/css/pokemon.css';
 import pokemonLogo from '../assets/International_Pokémon_logo.svg';
 import preLoader from '../assets/Poké_Ball_icon.svg';
 
+import colors from '../utils/pokemonColors';
+
 interface PropsType {
     pokemon: PokemonType,
     pokemonSpecies?: PokemonSpecies,
@@ -48,7 +50,7 @@ function PokemonImage({ pokemon, setLoaded }: PropsType) {
 
     return (
         <>
-            <div id='pokemon-image-div'>
+            <div id='pokemon-image-div' style={{backgroundColor: `${colors[pokemon.types[0].type.name as keyof typeof colors]}`}}>
                 <img ref={imageRef} width={400} className='pokemonImage' onLoad={showStats} src={pokemon.sprites.other['official-artwork'].front_default} />
             </div>
         </>
@@ -57,41 +59,49 @@ function PokemonImage({ pokemon, setLoaded }: PropsType) {
 
 function PokemonDiv({pokemon}: PropsType) {
     const [loaded, setLoaded] = useState(false);
+    const mainRef = useRef<HTMLElement>(null);
+
+    if(loaded) mainRef.current?.classList.add('visible');
     
     return (
         <>
             <div id='about-pokemon'>
+                {!loaded && <img src={preLoader} className='preLoader' />}
                 <section>
                     <div style={{textAlign: 'center'}}>
                         <img width={200} src={pokemonLogo} />
                     </div>
                 </section>
-                <section>
-                    <div>
-                        {loaded && <h1 id='pokemon-name' className={`type-color-${pokemon.types[0].type.name}`}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>}
-                    </div>
-                </section>
-                <section id='main-pokemon'>
+                <section ref={mainRef} id='main-pokemon' className='hidden'>
                     <div id='main-pokemon-flex'>
                         <div className='main-pokemon-flex-child'>
-                            {!loaded && <img src={preLoader} className='preLoader' />}
+                            <div id='pokemon-top-div'>
+                                <div>
+                                    <h1 id='pokemon-name' className={`type-color-${pokemon.types[0].type.name}`}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>
+                                </div>
+                                <div id='types-div'>
+                                    {pokemon.types.map(pokemonTypes => {
+                                        return (
+                                            <span key={pokemonTypes.type.name} className={`type-${pokemonTypes.type.name}`}>
+                                                {pokemonTypes.type.name.charAt(0).toUpperCase() + pokemonTypes.type.name.slice(1)}
+                                            </span>
+                                        )
+                                    })}
+                                </div>
+                            </div>
                             <PokemonImage pokemon={pokemon} setLoaded={setLoaded} />
                         </div>
                         <div className='main-pokemon-flex-child'>
-                            {loaded && <Stats pokemon={pokemon} />}
+                            <div id='tab-div'>
+                                <a>Stats</a>
+                                <a>Forms</a>
+                                <a>Weaknesses</a>
+                            </div>
+                            <Stats pokemon={pokemon} />
                         </div>
                     </div>
                 </section>
                 <section>
-                    <div id='types-div'>
-                        {pokemon.types.map(pokemonTypes => {
-                            return (
-                                <span key={pokemonTypes.type.name} className={`type-${pokemonTypes.type.name}`}>
-                                    {pokemonTypes.type.name.charAt(0).toUpperCase() + pokemonTypes.type.name.slice(1)}
-                                </span>
-                            )
-                        })}
-                    </div>
                 </section>
             </div>
         </>
---
+++ src/utils/pokemonColors.ts
@@ -0,0 +1,23 @@
+const colors = {
+    normal: '#aa98',
+    fire: '#f428',
+    water: '#39f8',
+    electric: '#fc38',
+    grass: '#7c58',
+    ice: '#6cf8',
+    fighting: '#b548',
+    poison: '#a598',
+    ground: '#db58',
+    flying: '#89f8',
+    psychic: '#f598',
+    bug: '#ab28',
+    rock: '#ba68',
+    ghost: '#66b8',
+    dragon: '#76e8',
+    dark: '#7548',
+    steel: '#aab8',
+    fairy: '#e9e8',
+    curse: '#6988',
+}
+
+export default colors;
